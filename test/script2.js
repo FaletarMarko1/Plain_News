2643a06de7b4a0435e07811d3e11bfdee6fe74b1
Marko <faletar.marko@gmail.com>
2023-06-19 21:02:27 +0200

    Made image dissapear and appear fixed everything

--- test/script2.js
+++ test/script2.js
@@ -1,13 +1,19 @@
 window.onload = function () {
-  let slika = document.getElementById("logo22");
+  let buton = document.getElementById("check");
+  function isHidden(buton) {
+    return buton.offsetParent === null;
+  }
+
+  hidden = isHidden(buton);
+
   window.addEventListener("scroll", scrollEffect);
-  let sirina = window.innerWidth;
+  slika = document.getElementById("logo22");
 
   function scrollEffect() {
-    if (window.scrollY > 0 && sirina < 768) {
+    if (window.scrollY > 20 ) {
       slika.style.opacity = "0";
       slika.style.transition = ".5s ease";
-    } else{
+    } else {
       slika.style.opacity = "1";
     }
   }
