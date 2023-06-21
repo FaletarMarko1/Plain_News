36edf03f014bea70c2d5aec3c1fcda5b1d150748
Marko Faletar <faletar.marko@gmail.com>
2023-06-21 10:53:28 +0200

    fixed file upload

--- skripta.php
+++ skripta.php
@@ -1,13 +1,62 @@
 <?php
-if (isset($_POST['title']) && isset($_POST['category']) && isset($_POST['slika']) && isset($_POST['about']) && isset($_POST['content']) && isset($_POST['submit'])) {
+if (isset($_POST['title']) && isset($_POST['category']) && isset($_POST['about']) && isset($_POST['content']) && isset($_POST['submit'])) {
     $naslov = $_POST['title'];
     $kategorija = $_POST['category'];
-    $slika = $_POST['slika'];
     $kratkiSadrzaj = $_POST['about'];
     $sadrzaj = $_POST['content'];
-    
+
 }
 
+$target_dir = "images/";
+$target_file = $target_dir . basename($_FILES["slika"]["name"]);
+$slikaName = ($_FILES["slika"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if (isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["slika"]["tmp_name"]);
+    if ($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["slika"]["size"] > 5000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+
+// Allow certain file formats
+if (
+    $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif"
+) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["slika"]["tmp_name"], $target_file)) {
+        echo "The file " . htmlspecialchars(basename($_FILES["slika"]["name"])) . " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -47,7 +96,7 @@ if (isset($_POST['title']) && isset($_POST['category']) && isset($_POST['slika']
             <section>
                 <article>
                     <div class="slike">
-                        <?php echo "<img src='images/$slika' alt='slika vijesti' />" ?>
+                        <?php echo "<img src='images/$slikaName' alt='slika vijesti' />" ?>
 
                     </div>
                     <div class="tekst">
